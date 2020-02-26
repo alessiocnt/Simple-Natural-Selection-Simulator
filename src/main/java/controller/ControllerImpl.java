8aaaa1fe359b600c6898fb5ff557c02316ab1f9c
Simone Ceredi <simone.ceredi@studio.unibo.it>
2020-02-26 11:20:46 +0100

    Created basic scheme for Simulation Loop

--- src/main/java/controller/ControllerImpl.java
+++ src/main/java/controller/ControllerImpl.java
@@ -1,9 +1,46 @@
 package controller;
 
+import settings.DayDuration;
+
 /**
  * Controller implementation.
  *
  */
-public class ControllerImpl implements Controller{
+public class ControllerImpl implements Controller {
 
+    private class SimulationLoop extends Thread {
+        private static final int UPDATES_IN_A_DAY = 100;
+        private volatile boolean running;
+
+        public SimulationLoop() {
+            this.running = true;
+        }
+
+        public void run() {
+            long lastTime = System.currentTimeMillis();
+            while (this.running /*TODO uncomment && !model.isSimulationOver()*/) {
+                final long current = System.currentTimeMillis();
+                final int elapsed = (int) (current - lastTime);
+                update();
+                render();
+                waitForNextFrame(dayDuration, elapsed);
+            }
+        }
+
+
+
+        private void update() {
+            // TODO Auto-generated method stub
+        }
+        private void render() {
+            // TODO Auto-generated method stub
+        }
+        private void waitForNextFrame(DayDuration dayDuration, int elapsed) {
+            try {
+                Thread.sleep((dayDuration.getDuration()/UPDATES_IN_A_DAY) - elapsed);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
