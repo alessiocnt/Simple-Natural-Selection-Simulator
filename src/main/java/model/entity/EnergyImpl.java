60a946e9b17f85bd5edaca5fb77e55834b6c104c
Alessio Conti <alessio.conti3@studio.unibo.it>
2020-02-27 13:12:31 +0100

    Changes to Energy and EnergyImpl

--- src/main/java/model/entity/EnergyImpl.java
+++ src/main/java/model/entity/EnergyImpl.java
@@ -31,9 +31,46 @@ public class EnergyImpl implements Energy {
      * {@inheritDoc}
      */
     @Override
-    public void setEnergy(final int energy) {
-        this.energyLevel = energy;
+    public void setEnergy(final Energy energy) {
+        this.energyLevel = energy.getEnergy();
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addEnergy(final Energy energy) {
+        this.energyLevel += energy.getEnergy();
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + energyLevel;
+        return result;
+    }
+
+    /**
+     * @param obj
+     *      the object to compare
+     * @return
+     *      true due to their energy level
+     *      false instead
+     */
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj == this) { 
+            return true; 
+        }
+        if (!(obj instanceof EnergyImpl)) { 
+            return false; 
+        } 
+        EnergyImpl c = (EnergyImpl) obj; 
+        return c.getEnergy() == this.getEnergy();
+    } 
 
     /**
      * toString for an Energy level.
