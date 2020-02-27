7822e39038fba30dce4d4ad29f3a12ab7dfa7fc7
Alessio Conti <alessio.conti3@studio.unibo.it>
2020-02-27 11:30:59 +0100

    Interface Organism refactor

--- src/main/java/model/entity/organism/Organism.java
+++ src/main/java/model/entity/organism/Organism.java
@@ -5,6 +5,7 @@ package model.entity.organism;
 
 import java.util.EnumMap;
 
+import model.entity.Entity;
 import model.mutation.Trait;
 import model.mutation.TraitType;
 
@@ -12,7 +13,7 @@ import model.mutation.TraitType;
  * Interface that defines an Organism.
  *
  */
-public interface Organism {
+public interface Organism extends Entity {
 
     /**
      * @return an EnumMap containing all organism's traits
