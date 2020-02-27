b6bc2394280e10ea595f23f4c2d136ceace1f532
Alessio Conti <alessio.conti3@studio.unibo.it>
2020-02-27 17:40:59 +0100

    Fix Food - remove position

--- src/main/java/model/entity/food/Food.java
+++ src/main/java/model/entity/food/Food.java
@@ -2,14 +2,11 @@ package model.entity.food;
 
 import model.entity.Energy;
 import model.entity.EntityImpl;
-import model.environment.position.Position;
 
 public class Food extends EntityImpl {
 
     private static final String EXCEPTIONMESSAGE = "Operation not supported for type Food.";
     /**
-     * @param position
-     *          The current Food position
      * @param energy
      *          The current Food energy
      */
