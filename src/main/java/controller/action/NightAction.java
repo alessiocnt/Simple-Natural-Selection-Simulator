1c8ee52a16b6dfe1d086fb5da0d4ab7592c88b71
Alessio Conti <alessio.conti3@studio.unibo.it>
2020-02-28 12:23:44 +0100

    Code NighAction and its strategy

---
+++ src/main/java/controller/action/NightAction.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package controller.action;
+
+import controller.action.strategy.ReplicateLogics;
+import controller.action.strategy.ReplicateLogicsImpl;
+import model.entity.Energy;
+import model.entity.organism.Organism;
+import model.environment.Environment;
+import model.environment.daycicle.DayPeriod;
+
+/**
+ * This class performs all the actions that occur in a night-time.
+ *
+ */
+public class NightAction extends AbstractAction {
+
+    private final ReplicateLogics replicateLogic;
+    private final Environment environment;
+
+    public NightAction(final Environment environment) {
+        super(DayPeriod.NIGHT);
+        this.environment = environment;
+        this.replicateLogic = new ReplicateLogicsImpl();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void perform(final Organism organism) {
+        if (!tryToRemoveOrganism(organism)) {
+            for (int i = 0; i < replicateLogic.getNumberOfChild(organism); i++) {
+                environment.addOrganism(organism, replicateLogic.replicate(organism));
+            }
+        }
+    }
+
+    /**
+     * @param organism the Organism
+     * @return True if the Energy is enough for replication
+     *         False instead.
+     */
+    private boolean isEnergyEnoughtToReplicate(final Organism organism) {
+        return Energy.greater(organism.getEnergy(), replicateLogic.computeConsumptionForReplication(organism))
+                || organism.getEnergy().equals(replicateLogic.computeConsumptionForReplication(organism));
+    }
+
+    /**
+     * @param organism the Organism to try to remove
+     * @return True if the Organism is removed.
+     *         False instead.
+     */
+    private boolean tryToRemoveOrganism(final Organism organism) {
+        if (!isEnergyEnoughtToReplicate(organism)) {
+            environment.removeOrganism(organism);
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/controller/action/strategy/ReplicateLogicsImpl.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package controller.action.strategy;
+
+import model.entity.Energy;
+import model.entity.organism.Organism;
+import model.mutation.TraitType;
+import model.mutation.factory.MutatedOrganismFactory;
+import model.mutation.factory.MutatedOrganismFactoryImpl;
+
+/**
+ * Class that defines the logic for for replicating an Organism.
+ *
+ */
+public class ReplicateLogicsImpl implements ReplicateLogics {
+
+    private final MutatedOrganismFactory duplicationFactory;
+
+    /**
+     * Constructor.
+     */
+    public ReplicateLogicsImpl() {
+        this.duplicationFactory = new MutatedOrganismFactoryImpl();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Organism replicate(final Organism organism) {
+        return duplicationFactory.createMutated(organism);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Energy computeConsumptionForReplication(final Organism organism) {
+        return organism.getTraits().get(TraitType.CHILDRENQUANTITY).getFoodConsumption(organism);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getNumberOfChild(final Organism organism) {
+        return organism.getTraits().get(TraitType.CHILDRENQUANTITY).getValue();
+    }
+}
